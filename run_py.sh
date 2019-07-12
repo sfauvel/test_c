41a3e71d38c1e1358c67b62f999c227bbceae7b8
sfauvel <sfauvel@gmail.com>
2019-07-12 17:03:22 +0200

    Add c program calling Python

---
+++ run_py.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function onDocker() {
+    command=$@
+    docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp gcc:4.9 $command
+}
+
+#docker run -v $(pwd)/test:/sources -w /sources -t python_dev ls
+#docker run -v $(pwd)/test:/sources -w /sources -t python_dev pip install cffi
+#docker run -v $(pwd)/test:/sources -w /sources -t python_dev whoami;python --version;sudo apt install python-pip;pip install cffi;python ./test/myapp.test.py
+
+onDocker make clean
+onDocker make callPython
+
+onDocker ./obj/callPython.o
+make
